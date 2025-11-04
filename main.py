46cc6fb06d3dd422be9631936d48f5397cf63da0
hruthika5a4 <131320755+hruthika5a4@users.noreply.github.com>
2025-11-04 10:25:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI
 from audit_checks import (
     check_compute_public_ips,
     check_sql_public_ips,
-    check_public_buckets(),
-    check_owner_service_accounts(),
-    check_gke_clusters()
+    check_public_buckets,
+    check_owner_service_accounts,
+    check_gke_clusters
 )
 
 app = FastAPI()
@@ -21,14 +21,14 @@ def get_public_ips():
 def get_sql_ips():
     return check_sql_public_ips()
 
-@app.get("/private_gke")
-def get_sql_ips():
+@app.get("/gke_public_endpoints")
+def get_gke_endpoints():
     return check_gke_clusters()
 
-@app.get("/sa_owner")
-def get_sql_ips():
+@app.get("/owner_sa")
+def get_owner_sa():
     return check_owner_service_accounts()
 
 @app.get("/public_buckets")
-def get_sql_ips():
+def get_public_buckets_route():
     return check_public_buckets()
